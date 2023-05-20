2d80d4c441098c99090bdd71d387f1559a4c4a1c
KeeHyuck <dlrlgurrns@gmail.com>
2023-05-20 15:54:28 +0900

    Solve 131. Palindrome Partitioning

---
+++ Backtracking/131. Palindrome Partitioning/__init__.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def isPalindrome(s):
+            str_len = len(s)
+            for i in range(0, str_len // 2):
+                if s[i] != s[str_len - 1 - i]:
+                    return False    
+            
+            return True
+        
+        def partitioning(res, cur, rest):
+            if not rest:
+                return res
+            
+            if isPalindrome(cur):
+                for 
+
+
+
+        for partition in range(1, len(s)):
+
+        
--- Backtracking/131. Palindrome Partitioning/index.js
+++ Backtracking/131. Palindrome Partitioning/index.js
@@ -3,5 +3,39 @@
  * @return {string[][]}
  */
 var partition = function(s) {
-    
+    const result = [];
+    const isPalindrome = (word) => {
+        const len = word.length;
+
+        for (let i = 0; i < Math.floor(len / 2); i += 1) {
+            if (word[i] !== word[len - 1 - i]) return false;
+        }
+        return true;
+    }
+
+    const search = (res, substring, len) => {
+        const target = substring.slice(0, len);
+        const restSubstring = substring.slice(len);
+
+        if (isPalindrome(target)) {
+            res = [...res, target]
+            for (let i=1; i <= restSubstring.length; i += 1) {
+                search(res, restSubstring, i);
+            }
+
+            if (!restSubstring) {
+                result.push(res);
+                return;
+            }
+        }
+    }
+
+    for (let i=1; i <= s.length; i += 1) {
+        search([], s, i);
+    }
+
+    return result;
 };
+
+
+console.log(partition("aaaab"));
