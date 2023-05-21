a95c8bcd07ce03b6cf2a6e86055c4ab70c6fa7f7
KeeHyuck <dlrlgurrns@gmail.com>
2023-05-21 10:46:07 +0900

    Solve 39. Combination Sum

--- Backtracking/39.Combination Sum/index.js
+++ Backtracking/39.Combination Sum/index.js
@@ -4,7 +4,31 @@
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
-    backtrack()
+    const results = [];
+    const combs = [];
+    
+    candidates.sort((a,b) => a - b);
+    const dfs = (start, total) => {
+        if (total === 0) {
+            results.push([...combs]);
+            return;
+        }
+
+        for (let i = start; i < candidates.length; i += 1) {
+            const candidate = candidates[i]
+
+            if (total - candidate >= 0) {
+                combs.push(candidate);
+                dfs(i, total - candidate);
+                combs.pop();
+            } else {
+                break;
+            }
+        }
+    }
+
+    dfs(0, target);
+    return results;
 };
 
-console.log(combinationSum([2], 8));
+console.log(combinationSum([3,2,7,6], 6));
