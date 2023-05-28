c1996f5191b6de182804152c10d4465384d0755b
KeeHyuck <dlrlgurrns@gmail.com>
2023-05-28 17:07:21 +0900

    Refactoring 684. Redundant Connection

--- Graph/684. Redundant Connection/__init__.py
+++ Graph/684. Redundant Connection/__init__.py
@@ -3,33 +3,32 @@ from typing import List
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        visited = set()
         n = len(edges)
-        adj = [[0 for i in range(n)] for j in range(n)]
+        parents = [i for i in range(n+1)]
+        rank = [1] * (len(edges) + 1)
+
+        def find(x):
+            if parents[x] != x:
+                parents[x] = find(parents[x])
+
+            return parents[x]
+
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+
+            if rootX == rootY:
+                return False
+            if rank[rootX] > rank[rootY]:
+                parents[rootY] = rootX
+            else:
+                parents[rootX] = rootY
+                rank[rootY] += 1
+            return True
 
         for start, end in edges:
-            adj[start-1][end-1] = 1
-            adj[end-1][start-1] = 1
-
-        def dfs(node):
-            visited.add(node)
-
-            for i in range(len(adj[node])):
-                if adj[node][i] == 1 and i not in visited:
-                    dfs(i)
-
-        for i in range(n-1, -1, -1):
-            start, end = edges[i]
-            adj[start-1][end-1] = 0
-            adj[end-1][start-1] = 0
-
-            dfs(i)
-            if len(visited) == n:
-                return edges[i]
-
-            adj[start-1][end-1] = 1
-            adj[end-1][start-1] = 1
-            visited = set()
+            if not union(start, end):
+                return [start, end]
 
 
 sol = Solution()
