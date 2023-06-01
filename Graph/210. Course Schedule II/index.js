5b4faf679a31285dfb510c734d6c668688f0fd28
KeeHyuck <dlrlgurrns@gmail.com>
2023-06-01 23:23:51 +0900

    Attempt 155. MinStack

--- Graph/210. Course Schedule II/index.js
+++ Graph/210. Course Schedule II/index.js
@@ -4,5 +4,37 @@
  * @return {number[]}
  */
 var findOrder = function(numCourses, prerequisites) {
-    
+    const adj = {};
+    const visited = new Array(numCourses).fill(false);
+
+    for (let i=0; i < numCourses; i +=1) {
+        adj[i] = [];
+    }
+
+    for (const [course, prerequisite] of prerequisites) {
+        adj[prerequisite].push(course);
+    }
+
+    const hasCycle = (visit) => {
+        console.log(visit);
+        if (visited[visit]) return true;
+        visited[visit] = true;
+
+        const courses = adj[visit] || []
+        for (const course of courses) {
+            if (hasCycle(course)) {
+                return true
+            }
+        }
+
+        visited[visit] = false;
+        return false;
+    }
+
+    // for (const key of Object.keys(adj)) {
+	// 	hasCycle(key);
+	// }
+    hasCycle(0)
 };
+
+findOrder(4, [[1,0],[2,0],[3,1],[3,2]]);
---
+++ Stack/155. MinStack/index.js
@@ -0,0 +1,43 @@
+var MinStack = function() {
+    this.stack = [];
+    this.min = null;
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+    this.min = Math.min(val, this.min);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.min;
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
